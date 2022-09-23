c5c46c14fa321baab685cd3906fb603e7eab8d3a
Francisca Mensah <franciscamensah181@gmail.com>
2022-09-23 01:21:41 +0000

    Tried to Refactor code

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
+require('dotenv').config();
 
 (async () => {
 
   // Init the Express application
-  const app = express();
+  const app: Express = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
@@ -32,26 +33,31 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   //! END @TODO1
 
 
-  app.get('/api/v0/filteredimage', async (req, res) => {
+  app.get('/api/v0/filteredimage', async (req: Request, res: Response) => {
     try {
       const { image_url } = req.query;
+
       if (!image_url) {
-        return res.status(404).send('Image URL required');
+        return res.status(400)
+          .send('Image URL required');
       }
-      console.log(req.query);
+
       const filtered_image_path = await filterImageFromURL(image_url);
-      // console.log(image_path)
-      res.status(200)
+
+      return res.status(200)
         .sendFile(filtered_image_path,
           () => { deleteLocalFiles([filtered_image_path]) })
-    } catch (error) {
-      res.status(500).send(error)
+    }
+    catch (error) {
+      console.log(error);
+      return res.status(500)
+        .send("Internal Server Error");
     }
   })
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   });
 
